0fb6a522a79b1e9a48878371dd4340de90a27290
WalterPython34 <morellosteve80@yahoo.com>
2025-07-08 12:33:48 -0500

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,19 +1,7 @@
-import { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
+const config = {
   appId: 'ai.nextax.startsmart',
   appName: 'StartSmart',
-  webDir: 'dist',
-  server: {
-    androidScheme: 'https',
-    iosScheme: 'https'
-  },
-  ios: {
-    scheme: 'StartSmart'
-  },
-  android: {
-    scheme: 'StartSmart'
-  }
+  webDir: 'dist'
 };
 
-export default config;
+module.exports = config;
